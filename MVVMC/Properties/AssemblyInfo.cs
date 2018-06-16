2a21fb5e81124b4f1983d6e36766d6c0353d02f4
Michael Shpilt <michaels@codevalue.net>
2018-06-16 14:16:30 +0300

    Adding .nuspec

--- MVVMC/Properties/AssemblyInfo.cs
+++ MVVMC/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MVVMC")]
+[assembly: AssemblyTitle("Wpf.MVVMC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MVVMC")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
