b5c4213569c642d5f6788525b7350eb2c1d8338d
Michael Shpilt <michaels@codevalue.net>
2018-06-18 19:25:18 +0300

    Making the sample app nicer with Material Design

--- MainApp/Pages/MainOperation/AboutViewModel.cs
+++ MainApp/Pages/MainOperation/AboutViewModel.cs
@@ -11,22 +11,12 @@ namespace MainApp.Pages.MainOperation
     {
         public string Text { get {
                 return 
-@"This project uses MVVMC, a lighweight infrastructure for navigation between views in WPF.
-
-The idea is to combine Controllers with MVVM that are responsible for navigation.
-
-Navigation areas & controllers:
-A navigation area is a Control which simply contains a content presenter with dynamic content.
-Each navigation area is controlled by a single controller.  
+@"This project uses MVVMC, a lighweight infrastructure for navigation between views in WPF. The idea is to combine Controllers with MVVM that are responsible for navigation.
 
 How navigation works:
-View, ViewModels or services can Request to navigate between screens. 
-This request calls a method in the specified controller, with specified page XXX as parameter.
-By default the controller will find a View called XXXView and a view model XXXViewModel, 
-connect them and show them as content.
+View, ViewModels or services can Request to navigate between screens. This request calls a method in the specified controller, with specified page XXX as parameter. By default the controller will find a View called XXXView and a view model XXXViewModel, connect them with DataContext and show them as content.
 
-The controller however can (and should) contain logic to process navigation requests and can
-do entirely different navigation according to context."; } }
+The controller however can (and should) contain logic to process navigation requests and can do entirely different navigation according to context."; } }
 
 
     }
