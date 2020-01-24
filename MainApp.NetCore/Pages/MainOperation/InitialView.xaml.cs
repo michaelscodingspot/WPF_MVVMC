40d7b9067c1a2c430069ea5449d5c0a6bf17dbeb
Michael Shpilt <michaels9876@gmail.com>
2020-01-24 16:44:05 +0200

    Add reference to MVVMC.NetCore

---
+++ MainApp.NetCore/Pages/MainOperation/InitialView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MainApp.NetCore.Pages.MainOperation
+{
+    /// <summary>
+    /// Interaction logic for InitialView.xaml
+    /// </summary>
+    public partial class InitialView : UserControl
+    {
+        public InitialView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainApp.NetCore/Pages/MainOperation/MainOperationController.cs
@@ -0,0 +1,18 @@
+﻿using MVVMC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp.NetCore.Pages.MainOperation
+{
+    public class MainOperationController : Controller
+    {
+        public override void Initial()
+        {
+            ExecuteNavigation();
+        }
+
+    }
+}
